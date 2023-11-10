1b372790430105a62dcc3d00efd235e3f0555e03
qecstr <yessenov.zhassulan@gmail.com>
2023-11-11 02:58:53 +0300

    adding a list of heroes and fixed paddings

--- src/main/java/com/example/animation/Heroes.kt
+++ src/main/java/com/example/animation/Heroes.kt
@@ -1,6 +1,16 @@
 package com.example.animation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring.DampingRatioLowBouncy
+import androidx.compose.animation.core.Spring.StiffnessVeryLow
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,26 +21,37 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.animation.Model.Hero
-
+import androidx.compose.foundation.lazy.itemsIndexed
 @Composable
 
 fun HeroCard(hero: Hero, modifier: Modifier = Modifier){
-    Card (modifier = Modifier.fillMaxWidth()){
+    Card (
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp,8.dp)
+        ){
         Row (modifier = Modifier.padding(16.dp)){
 
-            Column() {
+            Column(modifier = Modifier.weight(1f)) {
                 Text(
                     text = LocalContext.current.getString(hero.nameRes),
 
@@ -50,11 +71,27 @@ fun HeroCard(hero: Hero, modifier: Modifier = Modifier){
                 Image(
                     painter = painterResource(id = hero.imageRes ),
                     contentDescription = stringResource(id = hero.descriptionRes),
-                    modifier = Modifier.size(72.dp),
+                    modifier = Modifier
+                        .size(72.dp)
+                        .clip(RoundedCornerShape(8.dp)),
 
 
                 )
             }
+
+        }
+    }
+}
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun HeroList(list: List<Hero>,modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier,
+
+
+    ){
+        items(list){hero ->
+            HeroCard(hero = hero, modifier = modifier)
 
         }
     }
--- src/main/java/com/example/animation/MainActivity.kt
+++ src/main/java/com/example/animation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.animation.Data.HeroRepo
 import com.example.animation.Model.Hero
 import com.example.animation.ui.theme.SuperheroesTheme
 
@@ -43,11 +44,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SuperheroesTheme {
-        HeroCard(hero = Hero(
-            nameRes = R.string.hero1,
-            descriptionRes = R.string.description1,
-            imageRes = R.drawable.android_superhero1
-            )
-        )
+      HeroList(list = HeroRepo.heroes)
+
     }
 }
